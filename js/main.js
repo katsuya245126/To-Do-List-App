6041fa4e5030d8ac5269dd5c67f0c88741178531
John <katsuya245126@gmail.com>
2022-12-28 20:42:41 +0900

    Add app launcher code

--- js/main.js
+++ js/main.js
@@ -2,3 +2,10 @@ import ToDoList from "./todolist.js";
 import ToDoItem from "./todoitem.js";
 
 const toDoList = new ToDoList();
+
+// Launch app
+document.addEventListener("readystatechange", (event) => {
+    if (event.target.readyState === "complete") {
+        initApp();
+    }
+})
